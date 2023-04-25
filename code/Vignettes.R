11e64561770c38523be1949bef8394f6a0a80d18
Chris Stantis <chris.stantis@gmail.com>
2023-04-25 13:09:26 -0600

    Amending Vignette Maps

--- code/Vignettes.R
+++ code/Vignettes.R
@@ -1,19 +1,13 @@
 # Here we'll explore the four vignette cities only. Have to run 1 and 4 before this
-
+library(ggpubr)
 # Atlanta, which has small ranges and is multimodal
 
 hull_km_Atl_7 <- tapData_Atl_7 %>%
   group_by(km_cluster) %>%
   slice(chull(d18O, d2H))
 
-ggplot(data = tapData_Atl_7, aes(x = d18O, y = d2H)) + 
+p1 <- ggplot(data = tapData_Atl_7, aes(x = d18O, y = d2H)) + 
   geom_point(aes(fill = km_cluster), shape = 21, size = 3, alpha = 0.5) +
-  geom_smooth(data = tapData_Atl_7, 
-              aes(d18O, d2H, colour = "black"),
-              colour = "grey", method = "lm", size = 1, se = F) + 
-  stat_poly_eq( 
-    aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
-    parse = TRUE) +
   aes(fill = factor(km_cluster)) + 
   geom_polygon(data = hull_km_Atl_7, alpha = 0.5) + 
   labs(
@@ -30,14 +24,8 @@ hull_km_Law_14 <- tapData_Law_14 %>%
   group_by(km_cluster) %>%
   slice(chull(d18O, d2H))
 
-ggplot(data = tapData_Law_14, aes(x = d18O, y = d2H)) + 
+p2 <- ggplot(data = tapData_Law_14, aes(x = d18O, y = d2H)) + 
   geom_point(aes(fill = km_cluster), shape = 21, size = 3, alpha = 0.5) +
-  geom_smooth(data = tapData_Law_14, 
-              aes(d18O, d2H, colour = "black"),
-              colour = "grey", method = "lm", size = 1, se = F) + 
-  stat_poly_eq( 
-    aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
-    parse = TRUE) +
   aes(fill = factor(km_cluster)) + 
   geom_polygon(data = hull_km_Law_14, alpha = 0.5) + 
   labs(
@@ -54,14 +42,8 @@ hull_km_MPLS_21 <- tapData_MPLS_21 %>%
   group_by(km_cluster) %>%
   slice(chull(d18O, d2H))
 
-ggplot(data = tapData_MPLS_21, aes(x = d18O, y = d2H)) + 
+p3 <- ggplot(data = tapData_MPLS_21, aes(x = d18O, y = d2H)) + 
   geom_point(aes(fill = km_cluster), shape = 21, size = 3, alpha = 0.5) +
-  geom_smooth(data = hull_km_MPLS_21, 
-              aes(d18O, d2H, colour = "black"),
-              colour = "grey", method = "lm", size = 1, se = F) + 
-  stat_poly_eq( 
-    aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
-    parse = TRUE) +
   aes(fill = factor(km_cluster)) + 
   geom_polygon(data = hull_km_MPLS_21, alpha = 0.5) + 
   labs(
@@ -78,14 +60,8 @@ hull_km_Denv_15 <- tapData_Denv_15 %>%
   group_by(km_cluster) %>%
   slice(chull(d18O, d2H))
 
-ggplot(data = tapData_Denv_15, aes(x = d18O, y = d2H)) + 
+p4 <- ggplot(data = tapData_Denv_15, aes(x = d18O, y = d2H)) + 
   geom_point(aes(fill = km_cluster), shape = 21, size = 3, alpha = 0.5) +
-  geom_smooth(data = hull_km_Denv_15, 
-              aes(d18O, d2H, colour = "black"),
-              colour = "grey", method = "lm", size = 1, se = F) + 
-  stat_poly_eq( 
-    aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
-    parse = TRUE) +
   aes(fill = factor(km_cluster)) + 
   geom_polygon(data = hull_km_Denv_15, alpha = 0.5) + 
   labs(
@@ -95,7 +71,6 @@ ggplot(data = tapData_Denv_15, aes(x = d18O, y = d2H)) +
   scale_fill_manual(values = c("#003f5c", "#d2042d")) + 
   theme_classic() + 
   theme(legend.position = 'none')
-
 
 # Maps --------------------------------------------------------------------
 
@@ -110,16 +85,18 @@ atlantaWaterSources <- st_as_sf(atlantaWaterSources, coords = c("lon", "lat"),
                                  crs = 4326)
 
 tapData.sf_Atl_7$km_cluster <- tapData_Atl_7$km_cluster
-mapview(tapData.sf_Atl_7, 
+Atlmap <- mapview(tapData.sf_Atl_7, 
         zcol= 'km_cluster',
       #  color = "gray",          # outline color
         alpha.regions = 0.8,     # fill transparency
         alpha = 0.5, 
         col.regions = c("#003f5c", "#d2042d"), 
-        layer.name = "Cluster") + 
-  mapview(atlantaWaterSources, 
-          pch = c(17),
-          layer.name = "Municipal Water Source")
+        layer.name = "Cluster")
+
+
+mapviewOptions(fgb = FALSE)
+mapshot(Atlmap, url = paste0(getwd(), "/ATL.html"),
+        file = paste0(getwd(), "/ATL.png"))
 
 #Lawrence municipal water treatment centers include: 
 # Kaw River Water Treatment Plant (38.9808, -95.2408)
@@ -169,3 +146,11 @@ mapview(tapData.sf_Denv_15,
           alpha.regions = 0.8,     # fill transparency
           alpha = 0.5, 
           col.regions = c("#003f5c", "#d2042d"))
+
+
+# Putting It All Together -------------------------------------------------
+
+ggarrange(p1, p2, p3, p4, 
+ #         labels = c('Atlanta', 'Lawrence', 'Minneapolis', 'Denver'), 
+          ncol = 2, nrow = 2)
+
